bda201fc983b4d1d65a047f335bfe644a3068389
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-05-16 20:12:52 -0600

    Arrow

---
+++ Practica1/Arrow.js
@@ -0,0 +1,9 @@
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(3)); 
+console.log(cuadrado(5)); 
+console.log(cuadrado(2)); 
+
+
+//console.log
+//Muestra en consola en probando con 3 numero diferentes
--- Practica1/let_var.js
+++ Practica1/let_var.js
@@ -4,3 +4,7 @@ const edad = 25;
 nombre = "Ana Maria";
 
 const saludo = "Hola " + nombre + ", tienes " + edad + " años";
+
+console.log(saludo);  
+
+//muestra en consola en contenido de saludo 
