e5652ae6bc8b2e72bff56f6c38d320efd81c80ae
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-05-16 20:13:11 -0600

    SaludoPersonalizado

---
+++ Practica1/SaludoPersonalizado.js
@@ -0,0 +1,7 @@
+const presentacion = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años`;
+
+console.log(presentacion("Isay", 37));
+
+// crea un arrow function que reciba dos parametros: nombre y edad y retorne cada uno la siguiente 
+
+//"Hola, me llamo Isay y tengo 37 años"
