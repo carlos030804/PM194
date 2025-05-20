f9b81c0930fc4dc6d410f9991d6875c75c85a908
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-05-20 16:56:01 -0600

    Ejercicio B

---
+++ Practica2/EjercicioB.js
@@ -0,0 +1,10 @@
+const productos =[
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 }
+];
+
+const productosFiltrados = productos.filter(producto => producto.precio > 1000).map(producto => producto.nombre);
+
+console.log(productosFiltrados);
