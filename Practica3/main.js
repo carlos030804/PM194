418f22f8032fac904f82032576167906d1ec5a37
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-05-21 20:16:35 -0600

    utils main

---
+++ Practica3/main.js
@@ -0,0 +1,6 @@
+const { restar } = require('./utils');
+
+console.log(`5 - 3 = ${restar(5, 3)}`);       
+console.log(`10 - 7 = ${restar(10, 7)}`);     
+console.log(`100 - 150 = ${restar(100, 150)}`); 
+console.log(`0 - 0 = ${restar(0, 0)}`);  
